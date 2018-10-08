7607dc9be6ec0d522b5a5211b8af9e310329ab34
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 15:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter;
+  function addBump() {
+    counter+=1; 
+  }
+
+  function getBumps() {
+
+    return counter;
+  }
+
+  return {addBump, getBumps};
+}
